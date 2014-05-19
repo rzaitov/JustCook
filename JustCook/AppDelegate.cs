628e26b2d235d1eadd173f19dd264fa37b5cb90f
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2014-05-20 03:10:36 +0400

    Настроил vk-ios-sdk

--- JustCook/AppDelegate.cs
+++ JustCook/AppDelegate.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+
+using VK;
 
 namespace JustCook
 {
@@ -10,10 +13,17 @@ namespace JustCook
 	public partial class AppDelegate : UIApplicationDelegate
 	{
 		private UIWindow window;
+		private VkDelegate _vkDelegate;
+		VKApi _vkApi;
 
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+			_vkDelegate = new VkDelegate ();
+			VKSdk.InitializeWithDelegate(_vkDelegate, "4370991");
+
+			_vkApi = new VKApi ();
 
 			window.MakeKeyAndVisible();
 
