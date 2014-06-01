aa526850ba1eff30b58ef39076a0c7eebd0b1078
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2014-06-02 03:19:41 +0400

    Начал реализовывать лэйаут для картинок. Реализовал контэйнер который выравнивает изображения по высоте

---
+++ JustCook/Logic/ImagePacking/ImageContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Logic.Drawing;
+
+namespace Logic
+{
+	public abstract class ImageContainer
+	{
+		private readonly List<SizeF> _elements;
+		protected List<SizeF> Elements
+		{
+			get { return _elements;}
+		}
+
+		public abstract float Width { get; set; }
+		public abstract float Height { get; set; }
+
+		protected bool IsInitialized { get; set; }
+
+		public ImageContainer()
+		{
+			_elements = new List<SizeF> ();
+		}
+
+		public void Add(SizeF size)
+		{
+			_elements.Add(size);
+		}
+
+		protected void ThrowIfNotInitialized()
+		{
+			if (!IsInitialized)
+				throw new InvalidOperationException ();
+		}
+	}
+}
+
---
+++ JustCook/Logic/ImagePacking/RowContainer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Logic.Drawing;
+
+namespace Logic
+{
+	/// <summary>
+	/// Основная задача RowContainer – выравнивание элементов по высоте
+	/// </summary>
+	public class RowContainer : ImageContainer
+	{
+		private float _width;
+		private float _height;
+
+		public override float Width
+		{
+			get
+			{
+				ThrowIfNotInitialized();
+				return _width;
+			}
+			set
+			{
+				// После того как все элементы одной высоты их можно просто отмасштабировать
+				if (!IsInitialized)
+					Height = 100f;
+
+				float ratio = value / _width;
+				_height *= ratio;
+
+				// scale here
+				for (int i = 0; i < Elements.Count; i++)
+				{
+					SizeF size = Elements[i];
+					var scaledSize = new SizeF(_height, ratio * size.Width);
+					Elements[i] = scaledSize;
+				}
+
+				_width = value;
+			}
+		}
+
+		public override float Height
+		{
+			get
+			{
+				ThrowIfNotInitialized();
+				return _height;
+			}
+			set
+			{
+				_height = value;
+				_width = 0;
+
+				// scale here
+				for (int i = 0; i < Elements.Count; i++)
+				{
+					SizeF size = Elements[i];
+					float ratio = _height / size.Height;
+					var scaledSize = new SizeF(_height, ratio * size.Width);
+
+					Elements[i] = scaledSize;
+					_width += scaledSize.Width;
+				}
+
+				IsInitialized = true;
+			}
+		}
+
+		public RowContainer()
+		{
+		}
+	}
+}
+
