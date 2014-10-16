5e7af6f5da1a5ad35a485ce8b7739be2c3ceeefd
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-10-16 05:52:27 +0400

    added bookmark label, label for goup name and posted time label

--- JustCook/Screens/Recipe/RecipeViewController.designer.cs
+++ JustCook/Screens/Recipe/RecipeViewController.designer.cs
@@ -14,8 +14,40 @@ namespace JustCook
 	[Register ("RecipeViewController")]
 	partial class RecipeViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView Avatar { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView Bookmark { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel GroupTitle { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel Time { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (Avatar != null) {
+				Avatar.Dispose ();
+				Avatar = null;
+			}
+			if (Bookmark != null) {
+				Bookmark.Dispose ();
+				Bookmark = null;
+			}
+			if (GroupTitle != null) {
+				GroupTitle.Dispose ();
+				GroupTitle = null;
+			}
+			if (Time != null) {
+				Time.Dispose ();
+				Time = null;
+			}
 		}
 	}
 }
